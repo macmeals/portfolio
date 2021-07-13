ce28ff55542cbd1e779e7fa606fcd1bff76eec87
macmeals <loseworld9731@gmail.com>
2021-07-13 21:53:00 +0900

    work実績紹介までコーディング完了（CSS修飾）

--- js/image-pagenation.js
+++ js/image-pagenation.js
@@ -1,9 +1,11 @@
 $(function() {
 	$('.p-pagination').paginathing({//親要素のclassを記述
+		firstLast :false,
 		perPage: 3,//1ページあたりの表示件数
-		prevText:'<< previous',
-		nextText:'Next >>',//1つ次のページへ移動するボタンのテキスト
+		prevText:'<< Prev',
+		nextText:'Next>>',//1つ次のページへ移動するボタンのテキスト
 		ulClass: 'p-pagination__class',
+		containerClass:'p-pagination__container',
 	
 	})
 });

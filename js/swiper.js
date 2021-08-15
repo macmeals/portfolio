a82501cf226368735053760bef93b9e302967060
macmeals <loseworld9731@gmail.com>
2021-08-15 23:57:55 +0900

    swiperを実装 swiperの JS,SCSS(CSS)の整備

--- js/swiper.js
+++ js/swiper.js
@@ -1,21 +1,19 @@
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 2,
+    loop: true, //最後に達したら先頭に戻る
+    slidesPerView: 1,
     navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-    
+    },
+
     breakpoints: {
-      768: {
+      
+      699: {
         slidesPerView: 2
       },
-      980: {
-        slidesPerView: 3
-      },
       1200: {
-        slidesPerView: 4
+        slidesPerView: 3
       }
+    },
 
-      
-    }
-}
 });

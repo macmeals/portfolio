74cb4fe33b86066f2e4e842435629fac9e45d85a
macmeals <loseworld9731@gmail.com>
2021-08-16 23:12:40 +0900

    Service,  blog、WorkのSwiperの対応完了。 Contactまで完了。

--- js/image-pagenation.js
+++ js/image-pagenation.js
@@ -22,37 +22,3 @@ $(function() {
 
 });
 
-////////////////////////////////////////////////////////
-// $(window).on('load resize', function(){
-//   var winW = $(window).width();
-//   var devW = 1200;
-//   if (winW <= devW) {
-// 		//767px以下の時の処理
-		
-// 		$(function() {
-// 			$('.p-pagination').paginathing({//親要素のclassを記述
-// 				firstLast :false,
-// 				perPage: 2,//1ページあたりの表示件数
-// 				prevText:'<< Prev',
-// 				nextText:'Next>>',//1つ次のページへ移動するボタンのテキスト
-// 				ulClass: 'p-pagination__class',
-// 				containerClass:'p-pagination__container',
-			
-// 			})
-// 		})	
-
-//   } else {
-// 		//1200pxより大きい時の処理
-// 		$(function() {
-// 			$('.p-pagination').paginathing({//親要素のclassを記述
-// 				firstLast :false,
-// 				perPage: 3,//1ページあたりの表示件数
-// 				prevText:'<< Prev',
-// 				nextText:'Next>>',//1つ次のページへ移動するボタンのテキスト
-// 				ulClass: 'p-pagination__class',
-// 				containerClass:'p-pagination__container',
-			
-// 			})
-// 		})	
-//   }
-// });

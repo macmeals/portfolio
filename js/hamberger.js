4098b2c3fb42836eacb4e978bbda62db2f448241
macmeals <loseworld9731@gmail.com>
2021-08-22 14:14:31 +0900

    frontpageのハンバーガーメニューを完成

--- js/hamberger.js
+++ js/hamberger.js
@@ -1,6 +1,19 @@
 jQuery( function( $ ) {
 	$( ".p-hamberger" ).on( "click", function() {
-      $( ".p-frontside" ).toggleClass( "is-open" );
+		  $( ".p-frontside" ).toggleClass( "is-open" );
+			$( ".p-frontheader" ).toggleClass( "is-open" );
+			$( ".p-hambergermanu" ).toggleClass( "is-open" );
+			$( ".c-hambergerbackground" ).toggleClass( "is-open" );
       
 	} );
+
+	$( ".p-hambergermanu" ).on( "click", function() {
+		$( ".p-hambergermanu" ).toggleClass( "is-open" );
+		$( ".p-frontheader" ).toggleClass( "is-open" );
+		$( ".p-frontside" ).toggleClass( "is-open" );
+		$( ".c-hambergerbackground" ).toggleClass( "is-open" );
+} );
+
+
+	
 } );

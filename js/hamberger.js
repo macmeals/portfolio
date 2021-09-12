65752ccdc6be8d58882b9a623fed5192f5def43a
macmeals <loseworld9731@gmail.com>
2021-09-12 14:48:39 +0900

    frontpage, workのレスポンシブ（タブレット、スマートフォン）対応

--- js/hamberger.js
+++ js/hamberger.js
@@ -1,6 +1,7 @@
 jQuery( function( $ ) {
 	$( ".p-hamberger" ).on( "click", function() {
-		  $( ".p-frontside" ).toggleClass( "is-open" );
+			$( ".p-frontside" ).toggleClass( "is-open" );
+			$( ".p-workside" ).toggleClass( "is-open" );
 			$( ".p-frontheader" ).toggleClass( "is-open" );
 			$( ".p-hambergermanu" ).toggleClass( "is-open" );
 			$( ".c-hambergerbackground" ).toggleClass( "is-open" );
@@ -11,6 +12,7 @@ jQuery( function( $ ) {
 		$( ".p-hambergermanu" ).toggleClass( "is-open" );
 		$( ".p-frontheader" ).toggleClass( "is-open" );
 		$( ".p-frontside" ).toggleClass( "is-open" );
+		$( ".p-workside" ).toggleClass( "is-open" );
 		$( ".c-hambergerbackground" ).toggleClass( "is-open" );
 } );
 
